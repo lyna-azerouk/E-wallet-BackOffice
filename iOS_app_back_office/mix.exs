74742d392d0d39f1123855b7f22293919418730f
Lyna <azerouklyna2001@hotmail.com>
2024-11-14 20:11:28 +0100

    update svg

--- iOS_app_back_office/mix.exs
+++ iOS_app_back_office/mix.exs
@@ -59,7 +59,8 @@ defmodule IOSAppBackOffice.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:bcrypt_elixir, "~> 3.0"}
+      {:bcrypt_elixir, "~> 3.0"},
+      {:ex_heroicons, "~> 3.0.0"}
     ]
   end
 
